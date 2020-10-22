4f38baf3283000a2b71cb568ae844c6d01c20ca4
An1mus <an1mus@icloud.com>
2020-10-22 22:26:56 +0300

    API Key

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
 
+const YOUTUBE_API_KEY = 'AIzaSyCqOj5mhcbCBjPQyJUVVXn6bFQld29RbIc'
+
+const CHANNELS = [
+    {name: 'Прямой', url: 'https://www.youtube.com/c/%D0%A2%D0%B5%D0%BB%D0%B5%D0%BA%D0%B0%D0%BD%D0%B0%D0%BB%D0%9F%D1%80%D1%8F%D0%BC%D0%B8%D0%B9/videos'},
+    {name: 'Торонто', url: 'https://www.youtube.com/c/uttoronto/videos'},
+    {name: 'Ньюз Уан', url: 'https://www.youtube.com/channel/UC9oI0Du20oMOlzsLDTQGfug'},
+    {name: 'Украина 24', url: 'https://www.youtube.com/channel/UCMp5Buw-6LpbbV9r9Sl_5yg'},
+    {name: 'Наш', url: 'https://www.youtube.com/c/%D0%A2%D0%B5%D0%BB%D0%B5%D0%BA%D0%B0%D0%BD%D0%B0%D0%BB%D0%9D%D0%90%D0%A8/videos'}
+]
+
 const App = () => {
     return <>
-      <h1>Press "Spacebar" to switch next youtube channel</h1>
+        <h1>Press "Spacebar" to switch next youtube channel</h1>
+        <iframe src="https://www.youtube.com/embed/Li0t12Dcka0"/>
     </>;
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const CHANNELS = [
-    'https://www.youtube.com/c/%D0%A2%D0%B5%D0%BB%D0%B5%D0%BA%D0%B0%D0%BD%D0%B0%D0%BB%D0%9F%D1%80%D1%8F%D0%BC%D0%B8%D0%B9/videos',
-
-]
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
